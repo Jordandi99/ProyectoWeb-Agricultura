553aaddbc9a0e1f96cc7cc8c11f75710cc279367
erick-gm <erick_montanito@hotmail.com>
2021-11-28 00:28:13 -0600

    nuevos apartados

--- datosEscritor.php
+++ datosEscritor.php
@@ -14,7 +14,7 @@ $userN = $_SESSION['username'];
 <body>
 
     <nav class="navbar navbar-expand-md bg-dark navbar-dark">
-        <a class="navbar-brand" href="indexLector.php"><?php echo "Bienvenido $userN" ?> </a>
+        <a class="navbar-brand" href="indexEscritor.php"><?php echo "Bienvenido $userN" ?> </a>
 
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
             <span class="navbar-toggler-icon"></span>
@@ -23,10 +23,17 @@ $userN = $_SESSION['username'];
         <div class="collapse navbar-collapse justify-content-end" id="collapsibleNavbar">
             <ul class="navbar-nav">
                 <li class="nav-item">
-                    <a class="nav-link" href="datosLector.php">Información</a>
+                    <a class="nav-link" href="datosEscritor.php">Informacion</a>
                 </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="registroComentarios.php">Registro de comentarios</a>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="navbardrop" data-toggle="dropdown">
+                        Articulos
+                    </a>
+                    <div class="dropdown-menu">
+                        <a class="dropdown-item" href="agregarEscritor.php">Agregar</a>
+                        <a class="dropdown-item" href="publicadosEscritor.php">Publicados</a>
+                        <a class="dropdown-item" href="nopublicadosEscritor.php">No publicados</a>
+                    </div>
                 </li>
                 <li class="nav-item">
                     <a class="nav-link" href="include/salir.php">Cerrar Sesión</a>
@@ -34,24 +41,27 @@ $userN = $_SESSION['username'];
             </ul>
         </div>
     </nav>
-
-    <div class="card text-center">
+    <center>
+    <div class="card text-center" style="max-width: 1100px;">
         <div class="card-header">
-            Información de la cuenta
+            <h2>Información de la cuenta</h2>
         </div>
         <div class="card-body">
 
             <table class="table table-hover table-dark">
                 <thead>
                     <tr>
-                        <th scope="col">Nombre del lector</th>
-                        <th scope="col">Apellido</th>
-                        <th scope="col">Correo</th>
+                        <th scope="col">Nombre del escritor</th>
+                        <th scope="col">Apellido paterno</th>
+                        <th scope="col">Correo del escritor</th>
+                        <th scope="col">RFC</th>
                         <th scope="col">Edad</th>
                         <th scope="col">Genero</th>
-                        <th scope="col">Categoria de interes</th>
+                        <th scope="col">Direccion</th>
+                        <th scope="col">Telefono</th>
+                        <th scope="col">Referencias</th>
+                        <th scope="col">Categoria</th>
                         <th scope="col">Editar</th>
-
                     </tr>
                 </thead>
                 <tbody>
@@ -59,15 +69,17 @@ $userN = $_SESSION['username'];
                     <?php
 
                     //consulta para visualizar articulo
-                    $articulo = "SELECT L.id_lector, L.nombre,L.apeP,U.correo,L.edad,L.genero,L.tema_interes_cat
-                FROM usuarios U JOIN lector L
-                ON (U.id_usuario=L.id_usuario)
+                    $articulo = "SELECT E.id_escritor, E.nombre,E.apeP,U.correo,E.rfc,E.edad,E.genero,E.direccion,E.telefono,E.referencias,E.tema_interes_cat
+                FROM usuarios U JOIN escritor E
+                ON (U.id_usuario=E.id_usuario)
                 WHERE U.username='$userN'";
+
 
                     $resultado = mysqli_query($conexion, $articulo);
                     while ($mostrar = mysqli_fetch_row($resultado)) {
                         $datos = $mostrar[0] . "||" . $mostrar[1] . "||" . $mostrar[2] . "||" . $mostrar[3]
-                            . "||" . $mostrar[4] . "||" . $mostrar[5] . "||" . $mostrar[6];
+                            . "||" . $mostrar[4] . "||" . $mostrar[5] . "||" . $mostrar[6] . "||" . $mostrar[7]
+                            . "||" . $mostrar[8] . "||" . $mostrar[9] . "||" . $mostrar[10];
 
                     ?>
                         <tr>
@@ -78,6 +90,10 @@ $userN = $_SESSION['username'];
                             <td><?php echo $mostrar[4] ?></td>
                             <td><?php echo $mostrar[5] ?></td>
                             <td><?php echo $mostrar[6] ?></td>
+                            <td><?php echo $mostrar[7] ?></td>
+                            <td><?php echo $mostrar[8] ?></td>
+                            <td><?php echo $mostrar[9] ?></td>
+                            <td><?php echo $mostrar[10] ?></td>
 
                             <td>
                                 <button class="btn btn-warning" data-toggle="modal" data-target="#Meditar" onclick="agregaForm('<?php echo $datos ?>')">Editar</button>
@@ -92,25 +108,43 @@ $userN = $_SESSION['username'];
 
         </div>
         <div class="card-footer text-muted">
-            Datos del usuario
+            Datos del escritor
+        
         </div>
     </div>
+    </center>
     <!-- Editar -->
     <div class="modal fade" id="Meditar" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
         <div class="modal-dialog modal-sm" role="document">
-            <div class="modal-content">
+            <div class="modal-content" style="background: #2E2E2E;">
                 <div class="modal-header">
+                    <font color= "#FFFFFF">
                     <h5 class="modal-title" id="exampleModalLabel">Editar informacion</h5>
+                    </font>
                     <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                         <span aria-hidden="true">&times;</span>
                     </button>
                 </div>
                 <div class="modal-body">
                     <input type="text" hidden="" id="idAuto" name="">
+                    <font color= "#FFFFFF">
+                    <label for="marca">Dirección:</label>
+                    </font>
+                    <div class="form-group">
+                        <input type="text" class="form-control item" id="DireccionEdit" placeholder="Dirección">
+                    </div>
+                    <font color= "#FFFFFF">
+                    <label for="marca">Telefono:</label>
+                    </font>
+                    <div class="form-group">
+                        <input type="text" class="form-control item" id="telEdit" placeholder="Telefono">
+                    </div>
+                    <font color= "#FFFFFF">
                     <label for="marca">Categoria</label>
+                    </font>
                     <div class="form-group">
                         <select class="form-control item" id="categoriaEdit">
-                            <option value="0">Seleccione una opción</option>
+                            <option value="0" select>Seleccione una opción</option>
                             <option value="Maquinaria">Maquinaria</option>
                             <option value="Productos">Productos</option>
                             <option value="Plagas">Plagas</option>
@@ -139,16 +173,20 @@ $userN = $_SESSION['username'];
     function agregaForm(datos) {
         d = datos.split('||');
         $('#idAuto').val(d[0]);
-        $('#categoriaEdit').val(d[6]);
+        $('#DireccionEdit').val(d[7]);
+        $('#telEdit').val(d[8]);
+        $('#categoriaEdit').val(d[10]);
     }
 
     function actualizaDatos() {
         id = $('#idAuto').val();
+        direccion = $('#DireccionEdit').val();
+        telefono = $('#telEdit').val();
         categoria = $('#categoriaEdit').val();
-        cadena = "id=" + id + "&categoria=" + categoria;
+        cadena = "id=" + id + "&direccion=" + direccion + "&telefono=" + telefono + "&categoria=" + categoria;
         $.ajax({
             type: "POST",
-            url: "include/actualizarDatosLec.php",
+            url: "include/actualizaDatos.php",
             data: cadena,
             success: function(r) {
                 if (r == 1) {
@@ -164,8 +202,3 @@ $userN = $_SESSION['username'];
         });
     }
 </script>
-
-</body>
-
-
-</html>
